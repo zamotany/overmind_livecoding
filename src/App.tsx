00a8e8b5dd193fc16b01bfe057d8d3d8ad19b3d9
zamotany <zamotany.oss@gmail.com>
2020-01-26 17:33:10 +0100

    remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import cx from 'classnames';
-import * as tw from './styles/tailwind';
 import { useOvermind } from './overmind/overmind';
 
 export function App() {
@@ -9,16 +7,7 @@ export function App() {
   } = useOvermind();
 
   return (
-    <div
-      className={cx(
-        tw.w_full,
-        tw.h_full,
-        tw.flex,
-        tw.justify_center,
-        tw.items_center,
-        tw.bg_gray_200
-      )}
-    >
+    <div>
       <h1>{message}</h1>
     </div>
   );
